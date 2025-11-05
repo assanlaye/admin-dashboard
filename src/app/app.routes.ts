c34a7692be1183e21d40af180ed4a39ad70d22dd
Alassan Saine <assanlaye10@gmail.com>
2025-11-05 23:30:00 +0000

    Implemented components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Users } from './users/users';
+import { Products } from './products/products';
+import { Reports } from './reports/reports';
+import { Settings } from './settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'users', component: Users },
+  { path: 'products', component: Products },
+  { path: 'reports', component: Reports },
+  { path: 'settings', component: Settings },
+  { path: '**', redirectTo: '/dashboard' }
+];
